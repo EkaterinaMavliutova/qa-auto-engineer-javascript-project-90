b58dbd4b178ab68de7272273c27c69eb6bee6b2f
ekaterinamavliutova <akumaaya@gmail.com>
2024-10-16 16:51:23 +0200

    update tests in users.spec.js

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -29,9 +29,22 @@ test('should be possible to delete users from the table', async ({ page }) => {
   const logInPage = await startApp(page);
   const taskManager = await logInPage.logIn();
   const usersTab = await taskManager.goToUsersTab();
+  const usersBefore = await usersTab.usersTable.getItemsNumber();
+  const usersToDeleteCount = 2;
 
-  const selectedUsersIds = await usersTab.usersTable.selectUsersOnPage(2);
+  const selectedUsersIds = await usersTab.usersTable.selectItemsOnPage(usersToDeleteCount);
   const selectedUsersCount = await usersTab.usersTable.getSelectedItemsNumber();
-  await expect(selectedUsersCount).toBe(2);
+  await expect(selectedUsersCount).toBe(usersToDeleteCount);
+  await usersTab.usersTable.deletSelectedItems();
+  const usersAfter = await usersTab.usersTable.getItemsNumber();
+
+  for (const id of selectedUsersIds) {
+    await expect(await usersTab.usersTable.findItemById(id)).toBe('not found');
+  }
+  await expect(usersAfter).toBe(usersBefore - usersToDeleteCount);
+
+  // const userById = await usersTab.usersTable.findItemById('3');
+  // console.log('userById: ', userById);
+  //   await userById.click();
   // console.log(`!!!!!!!SelectedUsersCount: ${selectedUsersCount}`);
 });
