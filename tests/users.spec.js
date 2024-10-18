5fa6ff4910982d64b17f96d51d30f40796c07947
ekaterinamavliutova <akumaaya@gmail.com>
2024-10-18 10:44:52 +0200

    add tests to users.spec.js

--- tests/users.spec.js
+++ tests/users.spec.js
@@ -1,6 +1,13 @@
 import { expect, test } from '@playwright/test';
 import startApp from './utils';
 
+const userData = {
+  firstName: 'Sally',
+  lastName: 'Hanson',
+  email: 'sally@test.com',
+  password: 'secret',
+};
+
 test('should content at least 1 user', async ({ page }) => {
   const logInPage = await startApp(page);
   const taskManager = await logInPage.logIn();
@@ -16,7 +23,7 @@ test('all users on the page should have "First name", "Last name" and "Email"', 
   const usersTab = await taskManager.goToUsersTab();
 
   const usersData = await usersTab.usersTable.getTableData();
-
+  // console.log('!!!!!!!!!!tableData: ', usersData);
   usersData.forEach((user) => {
     expect(user['First name']).toBeTruthy();
     expect(user['Last name']).toBeTruthy();
@@ -48,3 +55,66 @@ test('should be possible to delete users from the table', async ({ page }) => {
   //   await userById.click();
   // console.log(`!!!!!!!SelectedUsersCount: ${selectedUsersCount}`);
 });
+
+test('should be possible to delete all users from the table', async ({ page }) => {
+  const logInPage = await startApp(page);
+  const taskManager = await logInPage.logIn();
+  const usersTab = await taskManager.goToUsersTab();
+  const usersCount = await usersTab.usersTable.getItemsNumber();
+
+  await usersTab.usersTable.selectAllItems();
+  const selectedUsersCount = await usersTab.usersTable.getSelectedItemsNumber();
+  await expect(selectedUsersCount).toBe(usersCount);
+  await usersTab.usersTable.deletSelectedItems();
+
+  await expect(usersTab.usersTable.tableComponent).not.toBeVisible();
+});
+
+test('should create new users', async ({ page }) => {
+  const logInPage = await startApp(page);
+  const taskManager = await logInPage.logIn();
+  const usersTab = await taskManager.goToUsersTab(); // деструктуризацию??? { usersTable }
+  const usersBefore = await usersTab.usersTable.getItemsNumber();
+  const newUserForm = await usersTab.usersTable.createNewItem(usersTab.editableFields);
+
+  await newUserForm.fillInputByLabel('Email', userData.email);
+  await newUserForm.fillInputByLabel('First name', userData.firstName);
+  await newUserForm.fillInputByLabel('Last name', userData.lastName);
+  await newUserForm.fillInputByLabel('Password', userData.password);
+  const newUserId = await newUserForm.saveItem();
+  await taskManager.goToUsersTab();
+  const usersAfter = await usersTab.usersTable.getItemsNumber();
+  const newUserData = await usersTab.usersTable.getItemDataById(newUserId);
+
+  // console.log('!!!!!!!!!!!!!!!!!!!newUserId: ', newUserId);
+
+  expect(newUserData).toMatchObject({ Email: userData.email });
+  expect(newUserData).toMatchObject({ 'First name': userData.firstName });
+  expect(newUserData).toMatchObject({ 'Last name': userData.lastName });
+  await expect(usersAfter).toBe(usersBefore + 1);
+});
+
+test('should not create user whithout data', async ({ page }) => {
+  const logInPage = await startApp(page);
+  const taskManager = await logInPage.logIn();
+  const usersTab = await taskManager.goToUsersTab();
+
+  const newUserForm = await usersTab.usersTable.createNewItem(usersTab.editableFields);
+
+  await expect(newUserForm.saveButton).toBeDisabled();
+});
+
+test('should edit user data', async ({ page }) => {
+  const logInPage = await startApp(page);
+  const taskManager = await logInPage.logIn();
+  const usersTab = await taskManager.goToUsersTab();
+
+  const userEditForm = await usersTab.usersTable.editItemById('1', usersTab.editableFields);
+  await userEditForm.fillInputByLabel('Last name', userData.lastName);
+  await userEditForm.saveItem();
+  const editedUser = await usersTab.usersTable.getItemDataById('1');
+
+  await expect(editedUser['Last name']).toEqual(userData.lastName);
+});
+
+
