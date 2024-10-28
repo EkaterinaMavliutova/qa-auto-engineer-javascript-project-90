6f10934aea7cb8bbfb4abeb4fdda3ee4e11134f3
ekaterinamavliutova <akumaaya@gmail.com>
2024-10-28 11:29:54 +0100

    refactor logIn.spec.js

--- tests/e2e/logIn.spec.js
+++ tests/e2e/logIn.spec.js
@@ -1,16 +1,19 @@
 import { expect, test } from '@playwright/test';
-import startApp from '../utils';
+import startApp from './utils/utils';
 
-test('should render logIn form', async ({ page }) => {
-  const logInPage = await startApp(page);
+let logInPage;
 
+test.beforeEach(async ({ page }) => {
+  logInPage = await startApp(page);
+});
+
+test('should render logIn form', async () => {
   await expect(logInPage.userNameInput).toBeVisible();
   await expect(logInPage.passwordInput).toBeVisible();
   await expect(logInPage.signInButton).toBeVisible();
 });
 
-test('should log in when username and password provided', async ({ page }) => {
-  const logInPage = await startApp(page);
+test('should log in when username and password provided', async () => {
   const taskManager = await logInPage.logIn();
 
   await expect(taskManager.fillerMessage).toBeVisible();
@@ -18,32 +21,28 @@ test('should log in when username and password provided', async ({ page }) => {
   await expect(taskManager.header.profileButton).toHaveText('Jane Doe');
 });
 
-test('should not log in when only username is provided', async ({ page }) => {
-  const logInPage = await startApp(page);
+test('should not log in when only username is provided', async () => {
   const taskManager = await logInPage.tryToLogIn('username');
 
   await expect(logInPage.errorMessage).toBeVisible();
   await expect(taskManager.fillerMessage).not.toBeVisible();
 });
 
-test('should not log in when only password is provided', async ({ page }) => {
-  const logInPage = await startApp(page);
+test('should not log in when only password is provided', async () => {
   const taskManager = await logInPage.tryToLogIn('password');
 
   await expect(logInPage.errorMessage).toBeVisible();
   await expect(taskManager.fillerMessage).not.toBeVisible();
 });
 
-test('should not log in when no username and password provided', async ({ page }) => {
-  const logInPage = await startApp(page);
+test('should not log in when no username and password provided', async () => {
   const taskManager = await logInPage.tryToLogIn();
 
   await expect(logInPage.errorMessage).toBeVisible();
   await expect(taskManager.fillerMessage).not.toBeVisible();
 });
 
-test('should be possible to log out', async ({ page }) => {
-  const logInPage = await startApp(page);
+test('should be possible to log out', async () => {
   const taskManager = await logInPage.logIn();
 
   await taskManager.logOut();
